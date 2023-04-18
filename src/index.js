204f5aa1833317edfd8d27725e290805689a99e6
Husen <saidinahusen10.hs@gmail.com>
2023-04-18 10:06:02 +0700

    counting w/ redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,41 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+
+// STORE
+// ACTION
+const increment = () => {
+  return {
+    type: "increment"
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "decrement"
+  }
+}
+// REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "increment": {
+      return {...state, counter: state.counter + 1}
+    }
+    case "decrement": {
+      return {...state, counter: state.counter - 1}
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const store = createStore(counter)
+store.subscribe(() => console.log(store.getState()))
+// DISPATCH
+store.dispatch(increment())
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
